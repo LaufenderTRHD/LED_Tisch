d78882c67021414dfbafd678a9170d1bb15d7342
doein <jan.doeinghaus@edu.fhdw.de>
2022-08-13 20:46:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/led_tisch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.led_tisch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
